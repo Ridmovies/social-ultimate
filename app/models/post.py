3089e78f8b88497eb6126ba3e8a9061cfd3e5a9d
riddler_rid <unkownrid@gmail.com>
2026-04-30 15:39:45 +0300

    + add config

---
+++ app/models/post.py
@@ -0,0 +1,15 @@
+# Минимум для поста:
+#
+# id
+# author_id (FK)
+# content
+# created_at
+# updated_at
+# возможно: is_deleted / visibility
+#
+# Сразу закладываются:
+#
+# индексы (author_id, created_at)
+# связи (relationship с User)
+
+
