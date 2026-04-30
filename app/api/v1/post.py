c0ff0051fb68997f144597fba53ab43634b715af
riddler_rid <unkownrid@gmail.com>
2026-04-30 16:31:10 +0300

    + add post router

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from app.schemas.post import PostBase, PostResponse
+from app.core.dependencies import get_db
+from app.models.post import Post
+from app.schemas.post import PostBase, PostResponse, PostCreate
 
 router = APIRouter(prefix="/post", tags=["post"])
 
@@ -8,5 +13,22 @@ router = APIRouter(prefix="/post", tags=["post"])
     "",
     #response_model=PostResponse
 )
-async def get_all_posts():
-    pass
+async def get_all_posts(
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(Post))
+    return result.scalars().all()
+
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=PostResponse,
+)
+async def create_post(
+        data: PostCreate,
+        db: AsyncSession = Depends(get_db),
+):
+    post = Post(**data.model_dump())
+    db.add(post)
+    await db.commit()
+    return post
