6a5d6262c4bd65f1e16f46814134d23e98d35a48
riddler_rid <unkownrid@gmail.com>
2026-04-30 17:00:31 +0300

    + add post restful api

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from app.core.dependencies import get_db
 from app.models.post import Post
-from app.schemas.post import PostBase, PostResponse, PostCreate
+from app.schemas.post import PostResponse, PostCreate, PostPatch, PostPut
 
-router = APIRouter(prefix="/post", tags=["post"])
+router = APIRouter(prefix="/posts", tags=["post"])
 
 @router.get(
     "",
-    #response_model=PostResponse
+    response_model=list[PostResponse]
 )
 async def get_all_posts(
     db: AsyncSession = Depends(get_db)
@@ -20,7 +20,7 @@ async def get_all_posts(
     return result.scalars().all()
 
 @router.post(
-    "/",
+    "",
     status_code=status.HTTP_201_CREATED,
     response_model=PostResponse,
 )
@@ -31,4 +31,72 @@ async def create_post(
     post = Post(**data.model_dump())
     db.add(post)
     await db.commit()
+    return post
+
+
+@router.get(
+    "/{post_id}",
+    response_model=PostResponse,
+)
+async def get_post_by_id(
+        post_id: int,
+        db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(Post).where(Post.id == post_id))
+    return result.scalars().one_or_none()
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+        post_id: int,
+        db: AsyncSession = Depends(get_db),
+):
+    stmt = delete(Post).where(Post.id == post_id)
+    await db.execute(stmt)
+    await db.commit()
+
+
+@router.put("/{post_id}", response_model=PostResponse)
+async def put_post(
+        post_id: int,
+        data: PostPut,
+        db: AsyncSession = Depends(get_db),
+) -> Post | None:
+
+    result = await db.execute(select(Post).where(Post.id == post_id))
+    post = result.scalars().one_or_none()
+
+    if not post:
+        return None
+
+    update_data = data.model_dump(exclude_unset=False)
+
+    for key, value in update_data.items():
+        setattr(post, key, value)
+
+    await db.commit()
+
+    return post
+
+
+@router.patch("/{post_id}", response_model=PostResponse)
+async def patch_post(
+        post_id: int,
+        data: PostPatch,
+        db: AsyncSession = Depends(get_db),
+) -> Post | None:
+
+    result = await db.execute(select(Post).where(Post.id == post_id))
+    post = result.scalars().one_or_none()
+
+    if not post:
+        return None
+
+    update_data = data.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(post, key, value)
+
+    await db.commit()
+
     return post
